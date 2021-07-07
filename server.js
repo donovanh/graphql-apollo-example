06e8e3606c38afaac496187f4a6f38295cf157fb
Donovan Hutchinson <donovanh@gmail.com>
2021-07-07 08:47:40 +0100

    Trying union types

--- server.js
+++ server.js
@@ -6,23 +6,43 @@ const db = require('./database.js');
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Actor {
-    id: String!,
-    name: String!,
+    id: String!
+    name: String!
     appearsIn: [Movie]
   }
 
   type Movie {
-    id: String!,
-    title: String!,
+    id: String!
+    title: String!
     starring: [Actor!]!
   }
 
+  type NewMovie {
+    id: String!
+    title: String!
+    starring: [String!]!
+  }
+
+  type Error {
+    id: String!,
+    message: String!
+  }
+
+  union NewMovieResponse = NewMovie | Error
+
   type Query {
-    actors: [Actor],
-    movies: [Movie],
-    actor(id: String): Actor,
-    movie(id: String): Movie,
-    moviesStarring(name: String!): [Movie]
+    actors: [Actor]
+    movies: [Movie]
+    actor(id: String!): Actor
+    movie(id: String!): Movie
+    moviesStarring(name: String!): [Movie]!
+  }
+
+  type Mutation {
+    createMovie(
+      title: String!,
+      starring: [String!]!
+    ): NewMovieResponse
   }
 `);
  
@@ -32,10 +52,24 @@ const root = {
   movies: () => Object.values(db.movies),
   actor: ({ id }) => db.actors[id],
   movie: ({ id }) => db.movies[id],
+  createMovie: ({ title, starring }) => {
+    // We could validate and update a database here
+    if (!title.length) {
+      return {
+        id: 'err-01',
+        message: 'Title should not be blank'
+      }
+    }
+    return {
+      id: 'new-id',
+      title,
+      starring
+    };
+  },
   moviesStarring: ({ name }) => {
     const actor = Object.values(db.actors).find((actor) => actor.name === name);
     return actor.appearsIn;
-  }
+  },
 };
  
 const app = express();
